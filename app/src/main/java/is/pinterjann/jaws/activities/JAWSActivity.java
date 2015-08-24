abde4101d354b8419e74a2a880cd3dd5374ee791
Jannis Pinter <jannis@pinterjann.is>
2015-08-24 20:56:58 +0200

    Added fake networks for screenshots

--- app/src/main/java/is/pinterjann/jaws/activities/JAWSActivity.java
+++ app/src/main/java/is/pinterjann/jaws/activities/JAWSActivity.java
@@ -45,6 +45,22 @@ public class JAWSActivity extends AppCompatActivity {
             wifiManager.setWifiEnabled(true);
         }
 
+        /* Fake networks for screenshots
+        List<WirelessNetwork> networkList = new ArrayList<>();
+        networkList.add(new WirelessNetwork("6E:AD:9E:53:02:B4", "Guest", 1, -80, "ESS OPEN", new Date().getTime()));
+        networkList.add(new WirelessNetwork("54:4F:7C:33:6B:4F", "Telcom", 5, -77, "ESS OPEN", new Date().getTime()));
+        networkList.add(new WirelessNetwork("CB:1A:07:DA:2D:EC", "Freifunk", 9, -58, "ESS OPEN", new Date().getTime()));
+        networkList.add(new WirelessNetwork("60:B0:56:A7:95:07", "Free Manning",13, -57, "ESS WPA", new Date().getTime()));
+        networkList.add(new WirelessNetwork("52:22:1E:E4:62:29", "Freedom", 100, -90, "ESS OPEN", new Date().getTime()));
+        networkList.add(new WirelessNetwork("F8:19:B0:58:CA:96", "private", 36, -67, "ESS WPA2", new Date().getTime()));
+        networkList.add(new WirelessNetwork("9D:44:E7:25:7A:CA", "JAWS 1", 40, -60, "ESS WEP", new Date().getTime()));
+        networkList.add(new WirelessNetwork("69:40:1E:93:1F:D3", "JAWS 2", 120, -72, "ESS WPA", new Date().getTime()));
+        networkList.add(new WirelessNetwork("7F:6D:12:88:D7:97", "JAWS 3", 1, -63, "ESS WPA2", new Date().getTime()));
+        networkList.add(new WirelessNetwork("49:97:33:B9:25:4E", "JAWS 4", 5, -59, "ESS WPA2 WPS", new Date().getTime()));
+        Collections.sort(networkList);
+        networkAdapter.setNetworkList(networkList);
+        */
+
         wifiScanReceiver = new WifiScanReceiver();
         registerReceiver(wifiScanReceiver, new IntentFilter(
                 WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
