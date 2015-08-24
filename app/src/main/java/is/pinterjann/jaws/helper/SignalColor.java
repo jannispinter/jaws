5f0b9d7c57398f388bbb342d08e476d24766992c
Jannis Pinter <jannis@pinterjann.is>
2015-08-24 20:17:10 +0200

    Removed dynamic color generation and added carefully chosen pastel colors instead

--- app/src/main/java/is/pinterjann/jaws/helper/SignalColor.java
+++ app/src/main/java/is/pinterjann/jaws/helper/SignalColor.java
@@ -7,18 +7,18 @@ public class SignalColor {
 
     public static int getColor(int signal) {
 
-        int signalPercent = WifiManager.calculateSignalLevel(signal, 100);
+        int signalPercent = WifiManager.calculateSignalLevel(signal, 5);
 
-        // Make sure that there is no division by zero
-        if(signalPercent == 0) {
-            signalPercent = 1;
+        /* carefully chosen pastel colors */
+        switch (signalPercent) {
+            case 4: /* fall through */
+            case 3: return Color.rgb(0, 106, 78);
+            case 2: return Color.rgb(255,179,71);
+            case 1: return Color.rgb(255, 105, 97);
+            case 0: return Color.rgb(194, 59, 34);
+
+            default: return Color.BLACK;
         }
-
-        int red = 255-(255/(100/signalPercent));
-        int green = 255/(100/signalPercent);
-        int blue = 0;
-
-        return Color.rgb(red, green, blue);
 
     }
 }
